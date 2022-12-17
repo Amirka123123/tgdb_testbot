f52804d1139ed6240e07206134937b1fe10e8efb
Amirka123123 <yakubovamirbek098@gmail.com>
2022-12-17 13:57:59 +0500

    We added buttons for tg bot

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import telebot
 import sqlite3
 
+from telebot.types import KeyboardButton, ReplyKeyboardMarkup
+
 TOKEN = "5810449448:AAHiFZR1N-ZaXZ3Ipa9GKseAeal-E9NQeig"
 
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
 
-@bot.message_handler(commands=['start', 'show_data'])
+@bot.message_handler(commands=['start'])
 def greetings(message):
     reply = "Hi! I'm a simple data collection bot"
-    bot.reply_to(message, reply)
-
-    if message.text == '/show_data':
-        data = read_data_from_db()
-        for datum in data:
-            bot.reply_to(message, str(datum))
+    bot.reply_to(message, reply, reply_markup=keyboard())
 
 
 is_taking_name = False
@@ -42,9 +39,14 @@ def message_handler(message):
         is_taking_surname = True
         bot.send_message(chat_id, "Input your Surname:")
 
-    if message.text == 'Save name':
+    if message.text == 'Save Name':
         is_taking_name = True
         bot.send_message(chat_id, "Input your Name:")
+
+    if message.text == 'Show Data':
+        data = read_data_from_db()
+        for datum in data:
+            bot.reply_to(message, str(datum))
 
 
 def save_data_to_db(name, surname):
@@ -55,7 +57,7 @@ def save_data_to_db(name, surname):
         connection = sqlite3.connect("botdatabase")
         cursor = connection.cursor()
 
-        inser_sql =n  f"""INSERT INTO
+        inser_sql =f"""INSERT INTO
                     User (name, surname) 
                     VALUES ('{name}', '{surname}')"""
 
@@ -87,5 +89,15 @@ def read_data_from_db():
         print("There was an error with database!")
         print(e)
 
+def keyboard():
+    markup = ReplyKeyboardMarkup(row_width=3)
+
+    button1 = KeyboardButton("Save Name")
+    button2 = KeyboardButton("Show Data")
+
+    markup.add(button1, button2)
+
+
+    return markup
 
 bot.infinity_polling()
