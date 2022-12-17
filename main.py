ffea8e1d4996be4ccb4d68737f3674177a580ee0
Amirka123123 <yakubovamirbek098@gmail.com>
2022-12-17 13:24:06 +0500

    We added database for telegram bot

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
 import telebot
+import sqlite3
 
 TOKEN = "5810449448:AAHiFZR1N-ZaXZ3Ipa9GKseAeal-E9NQeig"
 
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'show_data'])
 def greetings(message):
     reply = "Hi! I'm a simple data collection bot"
     bot.reply_to(message, reply)
 
+    if message.text == '/show_data':
+        data = read_data_from_db()
+        for datum in data:
+            bot.reply_to(message, str(datum))
+
 
 is_taking_name = False
 is_taking_surname = False
-is_taking_address = False
+user_name = None
+surname = None
 @bot.message_handler(content_types=['text'])
 def message_handler(message):
     chat_id = message.chat.id
     global is_taking_name
     global is_taking_surname
-    global is_taking_address
-
-    if is_taking_address == True:
-        address = message.text
-        print(address)
-        is_taking_address == False
+    global user_name
+    global surname
 
     if is_taking_surname == True:
         surname = message.text
         print(surname)
         is_taking_surname = False
+        save_data_to_db(user_name, surname)
 
     if is_taking_name:
         user_name = message.text
@@ -37,13 +41,51 @@ def message_handler(message):
         is_taking_name = False
         is_taking_surname = True
         bot.send_message(chat_id, "Input your Surname:")
-        is_taking_surname = False
-        is_taking_address = True
-        bot.send_message(chat_id, "Input your Address:")
 
     if message.text == 'Save name':
         is_taking_name = True
         bot.send_message(chat_id, "Input your Name:")
+
+
+def save_data_to_db(name, surname):
+
+    connection = None
+
+    try:
+        connection = sqlite3.connect("botdatabase")
+        cursor = connection.cursor()
+
+        inser_sql =n  f"""INSERT INTO
+                    User (name, surname) 
+                    VALUES ('{name}', '{surname}')"""
+
+        cursor.execute(inser_sql)
+        connection.commit()
+        connection.close()
+
+    except Exception as e:
+        print("There was an error in the database!")
+
+
+def read_data_from_db():
+    try:
+        connection = sqlite3.connect("botdatabase")
+        cursor = connection.cursor()
+
+        select_sql = """
+        SELECT * FROM User
+        """
+        cursor.execute(select_sql)
+        connection.commit()
+
+        data = cursor.fetchall()
+
+        connection.close()
+        return data
+
+    except Exception as e:
+        print("There was an error with database!")
+        print(e)
 
 
 bot.infinity_polling()
